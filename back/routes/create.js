1715f6b16ab53013f1910661e2c6cb58ef370914
vincent2021 <vincent2021@gmail.com>
2020-02-19 19:10:57 +0100

    create route

--- back/routes/create.js
+++ back/routes/create.js
@@ -2,11 +2,12 @@ const Router = require('express-promise-router')
 const db = require('../db')
 
 const router = new Router()
-
 module.exports = router
 
-router.post('/json', async (req, res) => {
-    const { rows } = await db.query('SELECT data FROM obj')
-    res.send(rows)
-})
+const test = {type:"Fiat", model:"500", color:"white"}
 
+router.post('/json', async (req, res) => {
+    console.log(req.body)
+    //const { rows } = await db.query('INSERT INTO obj(data) VALUES($1) RETURNING *', [test])
+    res.send('rows')
+})
