593c01e2a5856d64b311efb33c1c68de4fc98e07
bbouvard <eliott.bouvard@gmail.com>
2020-02-21 11:42:27 +0100

    Form loaded from opord

--- front/src/App.vue
+++ front/src/App.vue
@@ -11,27 +11,17 @@
     </template>
 
 <script>
-import VFormBase from 'vuetify-form-base';  
+  import VFormBase from 'vuetify-form-base';
+  import opord from './assets/opord.json';
+  import template from './assets/opord_template.json';
 
-    export default {	
-      components:{ VFormBase },
-      data () {
-        return {
-          myValue: {
-            name: 'Jumo',
-            password: '123456',
-            email: 'base@mail.com',
-            checkbox: true,
-            select: 'Jobs',
-          },   
-          mySchema: {
-            name: { type: 'text', label: 'Name' },
-            password: { type: 'password', label: 'Password' },
-            email: { type: 'email', label: 'Email' },
-            checkbox: { type: 'checkbox', label: 'Checkbox' },
-            select: { type: 'select', label: 'Select', items: ['Tesla', 'Jobs', 'Taleb'] }
-          }
-        }
+  export default {
+    components: { VFormBase },
+    data() {
+      return {
+        myValue: opord,
+        mySchema: template
       }
     }
+  }
 </script>
