acf94962980c90243047b2e35cb467847f7934b5
bbouvard <eliott.bouvard@gmail.com>
2020-02-21 11:21:51 +0100

    Basic form

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,60 +1,37 @@
-<template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
-</template>
+    <template>
+      <v-app>
+        <v-content>
+          <v-container fluid>
+            <v-form>
+              <v-form-base :value="myValue" :schema="mySchema" />
+            </v-form>
+          </v-container>   
+        </v-content>
+      </v-app>
+    </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import VFormBase from 'vuetify-form-base';  
 
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    export default {	
+      components:{ VFormBase },
+      data () {
+        return {
+          myValue: {
+            name: 'Jumo',
+            password: '123456',
+            email: 'base@mail.com',
+            checkbox: true,
+            select: 'Jobs',
+          },   
+          mySchema: {
+            name: { type: 'text', label: 'Name' },
+            password: { type: 'password', label: 'Password' },
+            email: { type: 'email', label: 'Email' },
+            checkbox: { type: 'checkbox', label: 'Checkbox' },
+            select: { type: 'select', label: 'Select', items: ['Tesla', 'Jobs', 'Taleb'] }
+          }
+        }
+      }
+    }
 </script>
