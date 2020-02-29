567fbed2858f5c6358efdd2d7b4595d7299a6013
bbouvard <eliott.bouvard@gmail.com>
2020-02-29 11:23:41 +0100

    Added drawer and navbar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,35 +1,32 @@
 <template>
-  <!-- App.vue -->
-
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
+    <v-navigation-drawer app mini-variant permanent>
+      <Menu></Menu>
     </v-navigation-drawer>
 
     <v-app-bar app>
-      <!-- -->
     </v-app-bar>
 
-    <!-- Sizes your content based upon application components -->
     <v-content>
-      <!-- Provides the application the proper gutter -->
       <v-container fluid>
-        <!-- If using vue-router -->
         <router-view></router-view>
       </v-container>
     </v-content>
 
     <v-footer app>
-      <!-- -->
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import Menu from "@/components/Menu.vue";
+
 export default {
   name: "App",
 
-  components: {},
+  components: {
+    Menu
+  },
 
   data: () => ({
     //
---
+++ front/src/components/Menu.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-list-item-group>
+    <v-list class="ma-0 pa-0" nav>
+      <v-list-item v-for="item in items" :key="item.title" @click="selectItem(item)">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+    </v-list>
+  </v-list-item-group>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data() {
+    return {
+      items: [
+        { title: "", icon: "", menu: "j0" },
+        { title: "J1", icon: "J1", menu: "j1" },
+        { title: "J2", icon: "J2", menu: "j2" },
+        { title: "J3", icon: "J3", menu: "j3" },
+        { title: "J4", icon: "J4", menu: "j4" },
+        { title: "J5", icon: "J5", menu: "j5" },
+        { title: "J6", icon: "J6", menu: "j6" },
+        { title: "J7", icon: "J7", menu: "j7" },
+        { title: "J8", icon: "J8", menu: "j8" },
+        { title: "J9", icon: "J9", menu: "j9" },
+        { title: "Je", icon: "JE", menu: "jE" }
+      ],
+      j0: null,
+      j1: null,
+      j2: null,
+      j3: null,
+      j4: null,
+      j5: null,
+      j6: null,
+      j7: null,
+      j8: null,
+      j9: null,
+      jE: null
+    };
+  },
+  methods: {
+    selectItem(item) {
+      this.$emit("item-selected", item);
+    }
+  }
+};
+</script>
