b8e44c258c79f5767063bf8438191ad81b6acf09
bbouvard <eliott.bouvard@gmail.com>
2020-02-29 12:56:32 +0100

    Added basic routing

---
+++ front/test/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app mini-variant permanent>
+      <Menu></Menu>
+    </v-navigation-drawer>
+
+    <v-app-bar app></v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script>
+import Menu from "@/components/Menu.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Menu
+  },
+
+  data: () => ({
+    //
+  }),
+
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
+};
+</script>
---
+++ front/test/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Form from '@/components/Form.vue'
+import List from '@/components/List.vue'
+import Viewer from '@/components/Viewer.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+	{ path: '/new/order', component: Form },
+	{ path: '/list/order', component: List },
+	{ path: '/view/order', component: Viewer }
+]
+
+const router = new VueRouter({
+	routes
+})
+
+export default router
