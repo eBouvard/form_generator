f35a7ab802d97b7b2867fd84305e38ecfdd12a41
bbouvard <eliott.bouvard@gmail.com>
2020-03-01 15:52:14 +0100

    Added style to form for testing purposes

--- front/src/components/FormComponents/FormComponent.vue
+++ front/src/components/FormComponents/FormComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid>
+  <v-container fluid style="border: 0.1rem solid grey; margin: 0.5rem;">
     <h2>{{items.label}}</h2>
     <v-text-field v-for="item in items.content" :key="item.label" :label="item.label"></v-text-field>
   </v-container>
