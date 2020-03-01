e7c8f792f78ede866e0b7fb7fc8e02d37898ddb6
bbouvard <eliott.bouvard@gmail.com>
2020-03-01 13:58:29 +0100

    Base for new form system

--- front/src/components/Form.vue
+++ front/src/components/Form.vue
@@ -1,55 +1,45 @@
 <template>
-    <v-container fluid>
-      <v-form id="to_send">
-        <v-text-field
-          label="Titre"
-          outlined
-          id="title"
-        ></v-text-field>
-        <v-form-base :value="myValue" :schema="mySchema" />
-      </v-form>
-      <v-btn block color="primary" @click="submitForm" dark>Submit</v-btn>
-    </v-container>   
+  <v-container fluid>
+    <v-form id="to_send">
+      <v-text-field v-for="item in template"
+          :key="item.label"
+          :label="item.label"
+      ></v-text-field>
+    </v-form>
+    <v-btn block color="primary" @click="submitForm" dark>Enregistrer</v-btn>
+  </v-container>
 </template>
 
 <script>
-	import VFormBase from 'vuetify-form-base';  
-	import template from '../assets/opord_template.json';  
-  import content from '../assets/opord.json';
-  import api from '@/service/api';
+import template from "../assets/opord_template.json";
+import api from "@/service/api";
 
-  export default {	
-    name: 'Form',
-    components:{ VFormBase },
-    data () {
-      return {
-        myValue: content,   
-        mySchema: template
-      }
-    },
-    methods: {
-      submitForm () {
-        var data = {
-          title: document.getElementById("title").value,
-          author: "",
-          date: "",
-          content: content
-        };
-        console.log(JSON.stringify(data));
-        api().post('/create/json', data).then((ret) => {
+export default {
+  name: "Form",
+  components: {},
+  data() {
+    return {
+      template: template
+    };
+  },
+  methods: {
+    submitForm() {
+      var data = {
+        title: document.getElementById("title").value,
+        author: "",
+        date: "",
+        content: ""
+      };
+      console.log(JSON.stringify(data));
+      api()
+        .post("/create/json", data)
+        .then(ret => {
           console.log(ret);
-        }).catch((e) => {console.log(e)});
-      }
+        })
+        .catch(e => {
+          console.log(e);
+        });
     }
   }
+};
 </script>
-
-<style>
-  #title { 
-    width:100%; 
-  }
-
-  #form-base .item { 
-    width:100%; 
-  }
-</style>
