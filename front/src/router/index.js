3b9ba7a5abfd53f6277bc4a302b8f664eccdd530
kantum <kantum@github.com>
2020-03-02 19:45:17 +0100

    added map

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -5,6 +5,7 @@ import Home from '@/components/Home.vue'
 import Form from '@/components/Form.vue'
 import Table from '@/components/Table.vue'
 import Viewer from '@/components/Viewer.vue'
+import Map from '@/components/Map.vue'
 
 Vue.use(VueRouter)
 
@@ -12,7 +13,8 @@ const routes = [
 	{ path: '/', component: Home },
 	{ path: '/new/order', component: Form },
 	{ path: '/list/order', component: Table },
-	{ path: '/view/order', component: Viewer }
+	{ path: '/view/order', component: Viewer },
+	{ path: '/map', component: Map }
 ]
 
 const router = new VueRouter({
