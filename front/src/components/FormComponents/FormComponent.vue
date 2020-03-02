af0288897a0d5fc539c43ce610a1e504fcc2b055
bbouvard <eliott.bouvard@gmail.com>
2020-03-02 14:23:20 +0100

    Generating mock form from template

--- front/src/components/FormComponents/FormComponent.vue
+++ front/src/components/FormComponents/FormComponent.vue
@@ -1,13 +1,25 @@
 <template>
-  <v-container fluid style="border: 0.1rem solid grey; margin: 0.5rem;">
-    <h2>{{items.label}}</h2>
-    <v-text-field v-for="item in items.content" :key="item.label" :label="item.label"></v-text-field>
+  <v-container fluid>
+    <div v-for="item in items" :key="item.label">
+      <div v-if="item.type === 'category'" style="border: 0.1rem solid grey; margin: 0.5rem;">
+        <CategoryComponent :item="item"></CategoryComponent>
+        <FormComponent :items="item.content"></FormComponent>
+      </div>
+      <TextComponent v-if="item.type === 'text'" :item="item"></TextComponent>
+    </div>
   </v-container>
 </template>
 
 <script>
+import CategoryComponent from "@/components/FormComponents/CategoryComponent.vue";
+import TextComponent from "@/components/FormComponents/TextComponent.vue";
+
 export default {
   name: "FormComponent",
+  components: {
+    CategoryComponent,
+    TextComponent
+  },
   data() {
     return {};
   },
---
+++ front/src/components/FormComponents/TextComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-text-field :label="item.label"></v-text-field>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "TextComponent",
+  data() {
+    return {};
+  },
+  props: {
+    item: Object
+  }
+};
+</script>
