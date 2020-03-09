a3426fdb24448cf38534a831baed08eef485fe1f
vincent2021 <vincent2021@gmail.com>
2020-03-09 09:51:40 +0100

    cleaning - api working

--- back/routes/index.js
+++ back/routes/index.js
@@ -2,10 +2,12 @@ const read = require('./read')
 const create = require('./create')
 const update = require('./update')
 const del = require('./delete')
+const parser = require('./parser')
 
 module.exports = app => {
   app.use('/read', read),
   app.use('/create', create),
   app.use('/update', update),
-  app.use('/delete', del)
+  app.use('/delete', del),
+  app.use('/parser', parser)
 }
--- back/routes/read.js
+++ back/routes/read.js
@@ -1,5 +1,5 @@
 const Router = require('express-promise-router')
-const db = require('../db')
+const db = require('../service/db')
 
 const router = new Router()
 module.exports = router
