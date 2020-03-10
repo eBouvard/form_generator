607fec3a1120fb6df6e814eaf254fcb5ebc2dce6
vincent2021 <vincent2021@gmail.com>
2020-03-10 17:03:10 +0100

    form2

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from 'vue-router'
 
 import Home from '@/components/Home.vue'
 import Form from '@/components/Form.vue'
-import Form2 from '@/components/Form2.vue'
 import Table from '@/components/Table.vue'
 import Viewer from '@/components/Viewer.vue'
 import Map from '@/components/Map.vue'
@@ -14,7 +13,6 @@ Vue.use(VueRouter)
 const routes = [
 	{ path: '/', component: Home },
 	{ path: '/new/order', component: Form },
-	{ path: '/new/order2', component: Form2 },
 	{ path: '/list/order', component: Table },
 	{ path: '/view/order/:form_id', component: Viewer },
 	{ path: '/map', component: Map },
