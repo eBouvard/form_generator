2d3d4b0d1073124da2206358b4ce2e874417d97f
bbouvard <eliott.bouvard@gmail.com>
2020-03-19 18:03:55 +0100

    Added autocomplete to security_classification

---
+++ front/src/components/FormComponents/AutoCompleteComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-autocomplete
+    :label="item.label"
+    :name="root"
+    v-model="root[value]"
+    :items="test"
+    item-text="name"
+  ></v-autocomplete>
+</template>
+
+<script>
+export default {
+  name: "AutoCompleteTextComponent",
+  data() {
+    return {
+      test: [
+        { name: 'Non Protégé', abbr: 'NP', id: 0 },
+        { name: 'Diffusion Restreinte', abbr: 'DR', id: 1 },
+        { name: 'Confidentiel-Défense', abbr: 'CD', id: 2 },
+        { name: 'Confidentiel-Défense Spécial France', abbr: 'CD-SF', id: 3 },
+        { name: 'Secret-Défense', abbr: 'SD', id: 4 },
+        { name: 'Secret-Défense Spécial France', abbr: 'SD-SF', id: 5 }
+      ]
+    };
+  },
+  props: {
+    item: Object,
+    root: Object,
+    value: String
+  }
+};
+</script>
