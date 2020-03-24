067d5f057b1ba232e8b38579285c1f08e48377d1
bbouvard <eliott.bouvard@gmail.com>
2020-03-24 12:18:32 +0100

    Added copy option to Table

--- front/src/components/Table.vue
+++ front/src/components/Table.vue
@@ -20,6 +20,11 @@
           <v-icon v-on:click="updateItem(item.id)">mdi-pencil-plus</v-icon>
         </v-btn>
       </template>
+      <template v-slot:item.copy="{ item }">
+        <v-btn class="ma-2" icon small right>
+          <v-icon v-on:click="copyItem(item.id)">mdi-content-copy</v-icon>
+        </v-btn>
+      </template>
       <template v-slot:item.delete="{ item }">
         <v-btn class="ma-2" icon small right>
           <v-icon v-on:click="deleteCheck = { check: true, id: item.id }">mdi-trash-can-outline</v-icon>
@@ -63,6 +68,7 @@ export default {
         { text: "Score", value: "score" },
         { text: "", value: "open" },
         { text: "", value: "modify" },
+        { text: "", value: "copy" },
         { text: "", value: "delete" }
       ],
       optionsList: [
@@ -123,6 +129,11 @@ export default {
     updateItem(id) {
       this.$router.push({
         path: "/update/order/" + id
+      });
+    },
+    copyItem(id) {
+      this.$router.push({
+        path: "/new/order/true/" + id
       });
     }
   },
