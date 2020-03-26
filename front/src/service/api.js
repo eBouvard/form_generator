76002afd62a2e8e2040c4a14de2055445abe2f5c
Eliott Bouvard <eliott.bouvard@gmail.com>
2020-03-26 12:14:18 +0000

    Update .gitlab-ci.yml, front/src/service/api.js files

--- front/src/service/api.js
+++ front/src/service/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 //Connection to Python API to parse the file
-let apiURL = 'http://localhost:3000'
+let apiURL = 'http://' + window.location.host.split(":")[0] + ':3000'
 
-if (process.env.NODE_ENV == 'production') {
-    apiURL = 'http://api:3000'
 }
 export default () => axios.create({
   baseURL: apiURL,
