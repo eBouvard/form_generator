fffdbb3d4e3781c94c037830d6cb42b05e292aa6
bbouvard <eliott.bouvard@gmail.com>
2020-03-26 13:34:05 +0100

    URL quicfix

--- front/src/service/api.js
+++ front/src/service/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 //Connection to Python API to parse the file
-let apiURL = 'http://' + window.location.host.split(":")[0] + ':3000'
+let apiURL = 'http://' + window.location.host.split(":")[0] + ':3000';
 
-}
 export default () => axios.create({
   baseURL: apiURL,
   headers: {
