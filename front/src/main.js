2462fab91f46adacdc028176af035c71dcbf4285
vincent2021 <vincent2021@gmail.com>
2020-03-26 15:25:01 +0100

    editor working

--- front/src/main.js
+++ front/src/main.js
@@ -7,11 +7,11 @@ import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 import '@/style/style.css'
-import Vue2TouchEvents from 'vue2-touch-events';
 import 'leaflet/dist/leaflet.css';
+import Vue2TouchEvents from 'vue2-touch-events';
 import JsonEditor from 'vue-json-edit'
 Vue.use(Vue2TouchEvents);
-Vue.use(JsonEditor)
+Vue.use(JsonEditor);
 
 Vue.config.productionTip = false
 
