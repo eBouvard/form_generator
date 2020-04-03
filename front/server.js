db6f84b8ce3b1b147be62bd716aca361a75f7adb
bbouvard <eliott.bouvard@gmail.com>
2020-04-04 01:38:47 +0200

    Serving with express

---
+++ front/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist/'));
+
+app.use(staticFileMiddleware);
+app.use(history());
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/' + '/index.html'));
+})
+
+app.listen(5000, function () {
+})
